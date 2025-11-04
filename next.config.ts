01f2dd29a5a7200f2a25d2e637594b48974984f7
Boobal <a.boobalzen003@gmail.com>
2025-11-04 18:27:52 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["@bug-reporter/shared"],
 };
 
 export default nextConfig;
